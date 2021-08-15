f1fa0a5e21d862e68a7628c287da679a8b6fcb86
Rune Nielsen <runenielsen@runbox.com>
2021-08-15 20:37:27 +0200

    now maps dawa unit and acess address status to keyword

--- src/danish_address_search_indexer/dawa.clj
+++ src/danish_address_search_indexer/dawa.clj
@@ -40,8 +40,48 @@
 (defn get-roads
   "Gets roads from DAWA."
   [transaction-id]
-  (->> (client/get (str dawa-base-url "/udtraek?entitet=navngivenvej&ndjson&txid=", transaction-id))
+  (->> (client/get (str dawa-base-url "/udtraek?entitet=navngivenvej&ndjson&txid=" transaction-id))
        parse-jsonline-response))
+
+(defn map-status
+  "Maps DAWA status code."
+  [status-code]
+  (case status-code
+    1 :active
+    2 :canceled
+    3 :pending
+    4 :discontinued
+    :not-valid))
+
+(defn map-unit-address
+  "Maps DAWA unit-address to domain representation."
+  [unit-address]
+  (into {:access-address-id (:adgangsadresseid unit-address)
+         :status (map-status (:status unit-address))
+         :floor (:etage unit-address)
+         :door (:dør unit-address)
+         :id (:id unit-address)
+         :created (:oprettet unit-address)
+         :updated (:ændret unit-address)}))
+
+(defn map-access-address
+  "Maps DAWA access-address to domain representation."
+  [access-address]
+  (into {:access-address-id (:adgangsadresseid access-address)
+         :status (map-status (:status access-address))
+         :id (:id access-address)
+         :road-code (:vejkode access-address)
+         :house-number (:husnr access-address)
+         :post-district-code (:postnr access-address)
+         :east-coordinate (:etrs89koordinat_øst access-address)
+         :north-coordinate (:etrs89koordinat_nord access-address)
+         :location-updated (:adressepunktændringsdato access-address)
+         :town-name (:supplerendebynavn access-address)
+         :plot-id (:matrikelnr access-address)
+         :road-id (:navngivenvej_id access-address)
+         :municipal-code (:kommunekode access-address)
+         :created (:oprettet access-address)
+         :updated (:ændret access-address)}))
 
 (defn- stream-jsonline-response
   [url f]
