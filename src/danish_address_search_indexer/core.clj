f9639dabc3af87be2ea300af4bc96818250de768
Rune Nielsen <runenielsen@runbox.com>
2021-08-22 21:11:58 +0200

    now bulks messages on channel

--- src/danish_address_search_indexer/core.clj
+++ src/danish_address_search_indexer/core.clj
@@ -1,34 +1,29 @@
 (ns danish-address-search-indexer.core
   (:require [danish-address-search-indexer.dawa :as dawa]
-            [clojure.core.async :refer [<! >!! go chan]]))
+            [clojure.core.async :refer [<! >!! go chan close!]]))
+
+(defn batch-xf [max-size]
+  (comp cat (partition-all max-size)))
 
 (defn import-access-addresses
   [transaction-id]
-  (let [println-ch (chan)]
-    (go (loop [n 0]
-          (when (zero? (mod n 10000))
-            (println n))
-          (dawa/map-access-address (<! println-ch))
-          (recur (inc n))))
+  (let [out (chan 1 (batch-xf 500))]
+    (go (loop [total 0]
+          (println total)
+          (when-let [addresses (<! out)]
+            (map dawa/map-access-address addresses)
+            (recur (+ total (count addresses))))))
     (dawa/get-all-access-addresses
      transaction-id
      (fn [dawa-acesss-address]
-       (>!! println-ch dawa-acesss-address)))))
-
-(defn import-unit-addresses
-  [transaction-id]
-  (dawa/get-all-unit-addresses
-   transaction-id
-   (fn [dawa-unit-address]
-     (let [unit-address (dawa/map-unit-address dawa-unit-address)]
-       (println unit-address)))))
+       (>!! out [dawa-acesss-address])))
+    (close! out)))
 
 (defn start-bulk-import
   []
   (let [latest-transaction (dawa/get-latest-transaction-id)
         transaction-id (:id latest-transaction)]
-    (time
-     (import-access-addresses transaction-id))))
+    (import-access-addresses transaction-id)))
 
 (defn -main []
   (start-bulk-import))
