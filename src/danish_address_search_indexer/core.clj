573e45d892cc07618381c47138614baa26e31f20
Rune Nielsen <runenielsen@runbox.com>
2021-08-23 21:17:21 +0200

    adds import unit address

--- src/danish_address_search_indexer/core.clj
+++ src/danish_address_search_indexer/core.clj
@@ -7,11 +7,18 @@
    transaction-id
    #(dawa/map-access-address %)))
 
+(defn import-unit-addresses
+  [transaction-id]
+  (dawa/get-all-unit-addresses
+   transaction-id
+   #(dawa/map-unit-address %)))
+
 (defn start-bulk-import
   []
   (let [latest-transaction (dawa/get-latest-transaction-id)
         transaction-id (:id latest-transaction)]
-    (import-access-addresses transaction-id)))
+    (import-access-addresses transaction-id)
+    (import-unit-addresses transaction-id)))
 
 (defn -main []
   (start-bulk-import))
